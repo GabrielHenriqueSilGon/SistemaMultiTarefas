ebaa263756c682c3d10511fed01d76e832389333
Gabriel H. S. Gontijo <84454532+GabrielHenriqueSilGon@users.noreply.github.com>
2022-09-19 18:58:40 -0300

    Update leitor.cpp

--- scr/leitor.cpp
+++ scr/leitor.cpp
@@ -16,7 +16,7 @@ leitor::LerArquivoD (unordered_map < string, vector < int >>*VD,
       while (!abreD.eof ())
 	{
 	  getline (abreD, s);
-	  TokenizarInColumn (s, VD, DD, cont);
+	  Tokenizar1 (s, VD, DD, cont);
 	  cont++;
 	}
     }
@@ -44,7 +44,7 @@ leitor::LerArquivoT (unordered_map < int, vector < string > >*VT,
       while (!abreT.eof ())
 	{
 	  getline (abreT, s);
-	  TokenizarInLine (s, VT, DT, cont);
+	  Tokenizar2 (s, VT, cont);
 	  cont++;
 	}
 
@@ -58,7 +58,7 @@ leitor::LerArquivoT (unordered_map < int, vector < string > >*VT,
 }
 
 void
-leitor::TokenizarInColumn (string s, unordered_map < string,
+leitor::Tokenizar1 (string s, unordered_map < string,
 			   vector < int > >*V, unordered_map < string,
 			   vector < int > >*D, int cont)
 {
@@ -85,9 +85,8 @@ leitor::TokenizarInColumn (string s, unordered_map < string,
 }
 
 void
-leitor::TokenizarInLine (string s, unordered_map < int,
-			 vector < string > >*V, unordered_map < string,
-			 vector < int > >*D, int cont)
+leitor::Tokenizar2 (string s, unordered_map < int,
+			 vector < string > >*V, int cont)
 {
   char vir = ',';
   string t;
@@ -103,6 +102,84 @@ leitor::TokenizarInLine (string s, unordered_map < int,
 	}
       conti++;
     }
+}
+
+
+
+void
+leitor::mostra (unordered_map < string, vector < int > >V)
+{
+
+  unordered_map < string, vector < int >>::iterator it;
+
+  for (it = V.begin (); it != V.end (); ++it)
+    {
+
+      cout << it->first << " :: ";
+
+    for (int n:it->second)
+	{
+
+	  cout << n << " ";
+	}
+
+      cout << endl;
+    }
+}
+
+void
+leitor::mostraVT (unordered_map < int, vector < string > >VT)
+{
+
+  unordered_map < int, vector < string > >::iterator it;
+
+
+  for (it = VT.begin (); it != VT.end (); ++it)
+    {
+
+      cout << it->first << " :: ";
+
+    for (string n:it->second)
+	{
+
+	  cout << n << " ";
+	}
+
+      cout << endl;
+    }
+}
+
+
+void
+leitor::Permuta (unordered_map < int, vector < string >> VT,
+		 unordered_map < string, vector < int >>VD)
+{
+  int cont = 0;
+  unordered_map < int, vector < string > >::iterator it;
+
+  unordered_map < int, vector < string > >VP;
+
+  cout << "\nValores de T encontrados como chave de D:\n";
+  for (it = VT.begin (); it != VT.end (); ++it)
+    {
+
+    for (string n:it->second)
+	{
+
+	  if (VD.find (n) == VD.end ())
+	    {
+
+	      cout << n << endl;
+	      VP[it->first].push_back (n);
+	      cont++;
+	    }
+	}
+    }
+
+  cout << "Total de chaves encontradas: " << cont << endl;
+
+  mostraVT (VP);
+
 }
 
 
